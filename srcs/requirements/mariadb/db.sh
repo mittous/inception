5e4c42d302ec0f89e6ced5a2ca152e779ab7158b
mittous <issmaimit123@gmail.com>
2023-08-12 19:55:32 +0100

    push from virtual machin

--- srcs/requirements/mariadb/db.sh
+++ srcs/requirements/mariadb/db.sh
@@ -3,9 +3,9 @@ sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS my_database;"
-mysql -e "CREATE USER IF NOT EXISTS 'mittous'@'%' IDENTIFIED BY '123';"
-mysql -e "GRANT ALL PRIVILEGES ON my_database.* to 'mittous'@'%';"
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* to '$DB_USER'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root shutdown
--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -7,39 +7,15 @@ cd /var/www/html/wordpress
 
 
 cat wp-config-sample.php > wp-config.php
-# wp config create \
-#     --dbname=my_database \
-#     --dbuser=mittous \
-#     --dbpass=123 \
-#     --dbhost=mariadb --allow-root --path=/var/www/html/wordpress
-# sed -i "s/define('DB_NAME', 'database_name_here');/define('DB_NAME', 'my_database');/g" /var/www/html/wordpress/wp-config.php
-# sed -i "s/define('DB_USER', 'username_here');/define('DB_USER', 'mittous');/g" /var/www/html/wordpress/wp-config.php
-# sed -i "s/define('DB_PASSWORD', 'password_here');/define('DB_PASSWORD', '123');/g" /var/www/html/wordpress/wp-config.php
-# sed -i "s/define('DB_HOST', 'localhost');/define('DB_HOST', 'mariadb');/g" /var/www/html/wordpress/wp-config.php
 
-# sed -i "s/define('DB_NAME', 'database_name_here');/define('DB_NAME', 'my_database');/" /var/www/html/wordpress/wp-config-sample.php
-# sed -i "s/define('DB_USER', 'username_here');/define('DB_USER', 'mittous');/" /var/www/html/wordpress/wp-config.php
-# sed -i "s/define('DB_PASSWORD', 'password_here');/define('DB_PASSWORD', '123');/" /var/www/html/wordpress/wp-config.php
-# sed -i "s/define('DB_HOST', 'localhost');/define('DB_HOST', 'mariadb');/" /var/www/html/wordpress/wp-config.php
-
-# chown -R www-data:www-data /var/www/html/wordpress
-# chmod -R 755 /var/www/html/wordpress
-
-wp config set DB_NAME my_database --allow-root --path=/var/www/html/wordpress
-wp config set DB_USER mittous --allow-root --path=/var/www/html/wordpress
-wp config set DB_PASSWORD 123 --allow-root --path=/var/www/html/wordpress
+wp config set DB_NAME $DB_NAME --allow-root --path=/var/www/html/wordpress
+wp config set DB_USER $DB_USER --allow-root --path=/var/www/html/wordpress
+wp config set DB_PASSWORD $DB_PASSWORD --allow-root --path=/var/www/html/wordpress
 wp config set DB_HOST mariadb --allow-root --path=/var/www/html/wordpress
 
-# wp core install \
-#     --url=localhost \
-#     --title=ft_server \
-#     --admin_user=mittous \
-#     --admin_password=123 \
-#     --allow-root --path=/var/www/html/wordpress
+wp core install --url=$URL --title=$MY_INCEPTION --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --skip-email --allow-root --path=/var/www/html/wordpress
 
-wp core install --url=localhost --title=ft_server --admin_user=mittous --admin_password=123 --admin_email=mit.tous@gmail.com --skip-email --allow-root --path=/var/www/html/wordpress
-
-wp user create wa7ed wa7ed@gmail.com --role=editor --user_pass=masso9ekch --allow-root --path=/var/www/html/wordpress
+wp user create $MY_USER $MY_EMAIL --role=editor --user_pass=$MY_PASS --allow-root --path=/var/www/html/wordpress
 
 mkdir -p /run/php
 

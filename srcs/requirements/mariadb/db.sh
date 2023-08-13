09fcf85274a021493fc945e1ab47e2628615be24
mittous <imittous@student.1337.ma>
2023-08-13 04:49:16 +0100

    from host to vm

--- srcs/requirements/mariadb/db.sh
+++ srcs/requirements/mariadb/db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-service mysql start
+service mariadb start
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
